b6b7d602e31e4644ce346355cb9ac14385a8c405
Sammy Phatlane <samuel@prologiq.co.za>
2022-09-28 09:18:49 +0200

    Spring Initialized app

---
+++ src/test/java/za/co/sammysa/Investec/InvestecApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.sammysa.Investec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
